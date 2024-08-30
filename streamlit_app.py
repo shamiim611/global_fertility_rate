1f69587c711c2e8249e2c65c5b83f530fb056970
shamiim611 <75389039+shamiim611@users.noreply.github.com>
2024-08-30 11:24:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Load the dataset from your GitHub repository
-url = 'https://github.com/shamiim611/global_fertility_rate/blob/main/fertility_rate.xls'
+url = 'https://raw.githubusercontent.com/shamiim611/global_fertility_rate/main/fertility_rate.xls'
 df = pd.read_excel(url,engine='xlrd')
 
 # Rename 'Country Name' to 'Country_Name'
